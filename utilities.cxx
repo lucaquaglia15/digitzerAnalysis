69e226d9ae5bfbbb2e3055987dde16844659f77a
Luca Quaglia <luca.quaglia@cern.ch>
2022-05-13 14:26:52 +0200

    Added .cxx and .h files

--- utilities.cxx
+++ utilities.cxx
@@ -1,4 +1,31 @@
 #include "utilities.h"
 
-void treeProducer () {
+void treeProducer (int numFiles, string folderPath, string trigFile, string waveFile) {
+
+	gSystem->cd(folderPath.c_str());
+
+	fstream dataFile;
+	string fileName;
+
+	for (int i = 0; i< numFiles; i++) {
+		fileName = waveFile + to_string(i) + ".txt";
+		dataFile.open(fileName.c_str());
+
+		if (dataFile.is_open()) { //Open file and parse useful information to put in .root file
+			cout << "Opening file: " << fileName << endl;
+		} 
+
+		else { //File opening not succesful, close the program
+			cout << "Error in opening wave file" << endl;
+			exit(0);
+		}
+
+		dataFile.close();
+
+	}
+}
+
+void countsConverter() {
+	//Open root file produced earlier
+
 }
--- utilities.h
+++ utilities.h
@@ -4,16 +4,28 @@
 #include "TTree.h"
 #include "TBranch.h"
 #include "TFile.h"
-
+#include "TSystem.h"
+#include "Riostream.h"
+#include <stdlib.h>
 #include <fstream>
 
-struct chamber {
-	std::string name = "ALICE-2-0";
-	int stripNum = 8, numSamp = 1024;
-	float sampFreq = 2.5; //Gs/s
+using namespace std;
+
+struct chamber { //CMS digitizer
+	std::string name = "ALICE-2-0"; 
+	int stripNum = 16, numSamp = 1024, resolution = 12, offsetBits = 16; //resolution and offsetBits are in bits
 	int strips[8] = {1,2,3,4,5,6,7,8};
+	float sampFreq = 2.5, vpp = 1000.0; //Gs/s, mV
+};
+struct chamberEPDT { //EPDT digitizer
+	std::string name = "ALICE-2-0";
+	int stripNum, numSamp;
+	int strips[7] = {1,2,3,4,5,6,7};
+	float sampFreq = 2.5; //Gs/s
 };
 
-void treeProducer();
+void treeProducer(int numFiles, string folderPath, string trigFile, string waveFile);
+
+void countsConverter();
 
 #endif
