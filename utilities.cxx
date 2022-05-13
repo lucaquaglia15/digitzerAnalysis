8293a0c2d15570df088240b9ae81bd38a8fc963c
Luca Quaglia <luca.quaglia@cern.ch>
2022-05-13 15:55:22 +0200

    Added gitignore file

--- utilities.cxx
+++ utilities.cxx
@@ -4,15 +4,21 @@ void treeProducer (int numFiles, string folderPath, string trigFile, string wave
 
 	gSystem->cd(folderPath.c_str());
 
-	fstream dataFile;
-	string fileName;
+	TFile *fData = new TFile("digitizerData.root","RECREATE"); //root file to save data
+	TTree *dataTree = new TTree("dataTree","Data from digitizer"); //tree
+
+	fstream dataFile; //For txt input file -> to stream data
+	string fileName; //Name of the input file
 
 	for (int i = 0; i< numFiles; i++) {
 		fileName = waveFile + to_string(i) + ".txt";
 		dataFile.open(fileName.c_str());
+		int lineCounter = 0;
 
 		if (dataFile.is_open()) { //Open file and parse useful information to put in .root file
 			cout << "Opening file: " << fileName << endl;
+
+			lineCounter++; //Increase line counter
 		} 
 
 		else { //File opening not succesful, close the program
